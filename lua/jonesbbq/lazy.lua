10004d66ad47b929f18d625db2b1acead0c820b9
Colson Xu <colson.xu@gmail.com>
2024-01-17 22:33:42 -0500

    added barbar.nvim

--- lua/jonesbbq/lazy.lua
+++ lua/jonesbbq/lazy.lua
@@ -42,5 +42,18 @@ require("lazy").setup({
   {'hrsh7th/cmp-nvim-lsp'},
   {'hrsh7th/nvim-cmp'},
   {'L3MON4D3/LuaSnip'},
+
+  -- barbar
+  {'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
 })
 
